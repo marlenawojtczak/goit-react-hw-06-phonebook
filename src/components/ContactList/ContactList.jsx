1467b882718b5d1fa26e9df59c119ad71deaca61
marlenawojtczak <wojtczakmarlena100@gmail.com>
2023-07-22 10:03:52 +0200

    template code

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -28,7 +28,7 @@ const ContactList = () => {
     <List>
       {filtered.map(contact => (
         <Item key={contact.id}>
-          {contact.name}: {contact.id}
+          {contact.name}: {contact.number}
           <Button
             type="button"
             name="delete"
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -25,7 +25,10 @@ const contactsSlice = createSlice({
 
     deleteContact(state, action) {
       const index = state.findIndex(contact => contact.id === action.payload);
-      state.splice(index, 1);
+      if (index !== -1) {
+        state.splice(index, 1);
+        localStorage.setItem('contacts', JSON.stringify(state));
+      }
     },
   },
 });
